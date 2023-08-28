6c97d611215f00f5b8028fc251c3cc4ba74b4d8c
Fayyaz Khan <140825109+mdfkh2708@users.noreply.github.com>
2023-08-29 01:34:09 +0530

    Update transcriptSummary.py

--- transcriptSummary.py
+++ transcriptSummary.py
@@ -5,8 +5,8 @@ model_name = "facebook/bart-large-cnn"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
-# Load the transcript file
-transcript_file = 'J_z-W4UVHkw_transcript.txt'
+# Load the transcript file generated using genTranscript.py
+transcript_file = 'example.txt'
 
 with open(transcript_file, 'r', encoding='utf-8') as f:
     transcript = f.read()
@@ -14,7 +14,7 @@ with open(transcript_file, 'r', encoding='utf-8') as f:
 # Tokenize and encode the transcript
 inputs = tokenizer.encode("summarize: " + transcript, return_tensors="pt", max_length=1024, truncation=True)
 
-# Generate the summary
+# Generate the summary of max 150 words or 1 liner
 summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
 summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
